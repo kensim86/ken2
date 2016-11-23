6aff988322941dab1a072aa63d77349e4bc35f3e
kenneth sim <kenneth.sim@techlyon.com>
2016-11-23 16:01:31 +0800

    try only hello and goodbye

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,34 +1,7 @@
 'use strict'
 
 exports.handle = function handle(client) {
-  const sayHello = client.createStep({
-    satisfied() {
-      return Boolean(client.getConversationState().helloSent)
-    },
-
-    prompt() {
-      client.addResponse('welcome')
-      client.addResponse('provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
-      client.updateConversationState({
-        helloSent: true
-      })
-      client.done()
-    }
-  })
-
-  const untrained = client.createStep({
-    satisfied() {
-      return false
-    },
-
-    prompt() {
-      client.addResponse('apology/untrained')
-      client.done()
-    }
-  })
+  
 
   const handleGreeting = client.createStep({
     satisfied() {
@@ -59,10 +32,7 @@ exports.handle = function handle(client) {
     },
     streams: {
       goodbye: handleGoodbye,
-      greeting: handleGreeting,
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained]
+      greeting: handleGreeting
     }
   })
 }
