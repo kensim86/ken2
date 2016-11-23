ece075a9a3e3700de45d75ce8cdbd0591e11865e
kenneth sim <kenneth.sim@techlyon.com>
2016-11-23 15:18:02 +0800

    try only greeting

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,35 +1,6 @@
 'use strict'
 
 exports.handle = function handle(client) {
-  const sayHello = client.createStep({
-    satisfied() {
-      return Boolean(client.getConversationState().helloSent)
-    },
-
-    prompt() {
-      client.addResponse('welcome')
-      client.addResponse('provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
-      client.updateConversationState({
-        helloSent: true
-      })
-      client.done()
-    }
-  })
-
-  const untrained = client.createStep({
-    satisfied() {
-      return false
-    },
-
-    prompt() {
-      client.addResponse('apology/untrained')
-      client.done()
-    }
-  })
-
   const handleGreeting = client.createStep({
     satisfied() {
       return false
@@ -41,24 +12,13 @@ exports.handle = function handle(client) {
     }
   })
 
-  const handleGoodbye = client.createStep({
-    satisfied() {
-      return false
-    },
-
-    prompt() {
-      client.addResponse('goodbye')
-      client.done()
-    }
-  })
-
   client.runFlow({
     classifications: {
-      goodbye: 'goodbye',
+      // Add a greeting handler with a reference to the greeting stream
       greeting: 'greeting'
     },
     streams: {
-      goodbye: handleGoodbye,
+      // Add a Stream for greetings and assign it a Step
       greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
